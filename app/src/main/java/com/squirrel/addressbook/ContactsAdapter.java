afa2912dec01438f634914dc6b903b627749cde2
tisqui <squiiirrel@gmail.com>
2015-12-07 23:58:50 -0800

    Fixed layout, added delete by swipe right in contacts list

--- app/src/main/java/com/squirrel/addressbook/ContactsAdapter.java
+++ app/src/main/java/com/squirrel/addressbook/ContactsAdapter.java
@@ -1,11 +1,15 @@
 package com.squirrel.addressbook;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -17,8 +21,10 @@ import java.util.List;
  * Created by squirrel on 12/5/15.
  */
 public class ContactsAdapter extends ArrayAdapter<Contact> {
+    private static final String LOG_TAG = ContactsAdapter.class.getSimpleName();
     private LayoutInflater mLayoutInflater;
     private static FragmentManager sFragmentManager;
+    private static final int DEFAULT_THRESHOLD = 200;
 
     public ContactsAdapter(Context context, FragmentManager fragmentManager){
         super(context, android.R.layout.simple_list_item_2);
@@ -63,19 +69,87 @@ public class ContactsAdapter extends ArrayAdapter<Contact> {
             }
         });
 
-        Button deleteBtn = (Button) view.findViewById(R.id.delete_button);
-        deleteBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ContactsDialog dialog = new ContactsDialog();
-                Bundle args = new Bundle();
-                args.putString(ContactsDialog.DIALOG_TYPE, ContactsDialog.DELETE_CONTACT);
-                args.putInt(ContactsContract.ContactsColumns.CONTACTS_ID, id);
-                args.putString(ContactsContract.ContactsColumns.CONTACT_NAME, name);
-                dialog.setArguments(args);
-                dialog.show(sFragmentManager, "delete-contact");
-            }
-        });
+//        Button deleteBtn = (Button) view.findViewById(R.id.delete_button);
+//        deleteBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ContactsDialog dialog = new ContactsDialog();
+//                Bundle args = new Bundle();
+//                args.putString(ContactsDialog.DIALOG_TYPE, ContactsDialog.DELETE_CONTACT);
+//                args.putInt(ContactsContract.ContactsColumns.CONTACTS_ID, id);
+//                args.putString(ContactsContract.ContactsColumns.CONTACT_NAME, name);
+//                dialog.setArguments(args);
+//                dialog.show(sFragmentManager, "delete-contact");
+//            }
+//        });
+
+            view.setOnTouchListener(new View.OnTouchListener() {
+
+                int initialX = 0;
+                final float slop = ViewConfiguration.get(getContext()).getScaledTouchSlop();
+
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    int showedDialog = 0;
+
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        initialX = (int) event.getX();
+                        Log.d(LOG_TAG,"InitialX = " + initialX);
+                        view.setPadding(0, 0, 0, 0);
+                        showedDialog = 0;
+                        Log.d(LOG_TAG, "Padding set to 0,0,0,0 ");
+
+                    } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                        int currentX = (int) event.getX();
+                        int offset = currentX - initialX;
+
+                        if (Math.abs(offset) > slop && offset > 0) {
+                            view.setPadding(offset, 0, 0, 0);
+                            Log.d(LOG_TAG, "Padding set to "+offset+" ,0,0,0 ");
+
+                            if (offset >= DEFAULT_THRESHOLD && showedDialog ==0 ) {
+                                Log.d(LOG_TAG, "Offset = " + offset + " > " + DEFAULT_THRESHOLD);
+                                showedDialog++;
+                                // Left to right - delete action
+                                ContactsDialog dialog = new ContactsDialog();
+                                Bundle args = new Bundle();
+                                args.putString(ContactsDialog.DIALOG_TYPE, ContactsDialog.DELETE_CONTACT);
+                                args.putInt(ContactsContract.ContactsColumns.CONTACTS_ID, id);
+                                args.putString(ContactsContract.ContactsColumns.CONTACT_NAME, name);
+                                dialog.setArguments(args);
+                                dialog.show(sFragmentManager, "delete-contact");
+
+                                //animate back
+                                ValueAnimator animator = ValueAnimator.ofInt(view.getPaddingLeft(), 0);
+                                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                                    @Override
+                                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                                        view.setPadding((Integer) valueAnimator.getAnimatedValue(), 0, 0, 0);
+                                    }
+                                });
+                                animator.setDuration(150);
+                                animator.start();
+
+                            } else if (offset < - DEFAULT_THRESHOLD) {
+                                //Do nothing on swipe left
+                            }
+                        }
+                    } else if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_CANCEL) {
+                        // Animate back if no action was performed.
+                        ValueAnimator animator = ValueAnimator.ofInt(view.getPaddingLeft(), 0);
+                        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                                view.setPadding((Integer) valueAnimator.getAnimatedValue(), 0, 0, 0);
+                            }
+                        });
+                        animator.setDuration(150);
+                        animator.start();
+                    }
+
+                    return false;
+                }
+            });
 
         return view;
     }
